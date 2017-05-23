d1a524a5b44d1e80af004a61e7e24e2e1ffaa1ff
vit-aborigen <vit.aborigen@gmail.com>
2017-05-23 10:08:33 +0300

    formatting

--- Junk/Battleship.py
+++ Junk/Battleship.py
@@ -1,17 +1,29 @@
-import string
+import string, random
+import numpy as np
 
 class Board():
-    def __init__(self, size, ship_config):
-        self.board = [['O' for _ in range(size+1)] for j in range(size+1)]
+    def __init__(self, size, ships):
+        self.board = [['.' for _ in range(size+1)] for j in range(size+1)]
         for i in range(size+1):
             self.board[0][i] = string.ascii_letters[i-1].upper()
             self.board[i][0] = i
-        self.ships = ship_config
+        self.ships = ships
 
+    # def __str__(self):
+    #     s = [[str(i) for i in row] for row in self.board]
+    #     lens = [max(map(len, col)) for col in zip(*s)]
+    #     fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+    #     table = [fmt.format(*row) for row in s]
+    #     return '\n'.join(table)
 
-    def print_board(self):
-        for row in self.board:
-            print(row)
+    def __str__(self):
+        s = [[str(i) for i in row] for row in self.board]
+        print(s)
 
-player = Board(3, {})
-player.print_board()
+        return '\n'.join(s)
+
+    def __repr__(self):
+        return self.__str__()
+
+player = Board(8, {})
+print(player)
