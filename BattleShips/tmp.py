f38e97f8247ada39014ddcf9bb9903c9dc643aef
vit-aborigen <vit.aborigen@gmail.com>
2017-05-27 11:53:43 +0300

    checking range fixed

--- BattleShips/tmp.py
+++ BattleShips/tmp.py
@@ -14,12 +14,10 @@ def place_sheep(ship_size, board):
         :return: True if empty, False otherwise
         """
         x,y = coord
-        if board[x][y] != '.':
-            return False
 
         #checking cells around ship's square considering borders
-        check_range_x = [x - 1 if x > 0 else x, x + 1 if x < SIZE else x]
-        check_range_y = [y - 1 if y > 0 else y, y + 1 if y < SIZE else y]
+        check_range_x = [x - 1 if (x-1) > 0 else x, x + 1 if (x+1) < SIZE else x]
+        check_range_y = [y - 1 if (y-1) > 0 else y, y + 1 if (y+1) < SIZE else y]
         if orientation == 'horizontal':
             check_range_x[1] += ship_size
             if check_range_x[1] > SIZE-1:
@@ -29,26 +27,28 @@ def place_sheep(ship_size, board):
             if check_range_y[1] > SIZE - 1:
                 check_range_y[1] = SIZE - 1
 
-
-        print("Checking range for ship:", ship_size, check_range_x, check_range_y)
+        print("Checking range for ship:", ship_size, check_range_x, check_range_y, orientation)
         #check all squares around whole ship
-        for x in check_range_x:
-            for y in check_range_y:
+        for x in range(check_range_x[0], check_range_x[1]+1):
+            for y in range(check_range_y[0], check_range_y[1]+1):
                 if board[x][y] != '.': return False
         return True
 
     # Generate random position (left top corner)
-    orientation = 'horizontal' if random.randint(0, 1) else 'vertical'
-    x_random = random.randint(0, SIZE - ship_size)
-    y_random = random.randint(0, SIZE - ship_size)
-    while not is_fit(ship_size, board, (x_random,y_random), orientation):
-        orientation = 'horizontal' if random.randint(0,1) else 'vertical'
-        x_random = random.randint(0, SIZE-ship_size)
-        y_random = random.randint(0, SIZE-ship_size)
+    isFit = False
+    while not isFit:
+        orientation = 'horizontal' if random.randint(0, 1) else 'vertical'
+        if orientation == 'horizontal':
+            x_random = random.randint(0, SIZE - 1)
+            y_random = random.randint(0, SIZE - ship_size)
+        else:
+            x_random = random.randint(0, SIZE - ship_size)
+            y_random = random.randint(0, SIZE - 1)
+        print('Coordinates generated', (x_random, y_random))
+        isFit = is_fit(ship_size, board, (x_random,y_random), orientation)
 
     # Check position
-    # if is_fit(ship_size, board, (x_random,y_random), orientation):
-    if orientation == 'horizontal':
+    if orientation == 'vertical':
         for x in range(x_random, x_random+ship_size):
             board[x][y_random] = str(ship_size)
     else:
@@ -59,7 +59,7 @@ def place_sheep(ship_size, board):
         print(' '.join(row))
     print('\n')
 
-ships = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
+ships = [4, 4]
 board = [['.']*SIZE for _ in range(SIZE)]
 for ship in ships:
     place_sheep(ship, board)
