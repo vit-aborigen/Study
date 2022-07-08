0a0deedad425d0ee50238bf58adca4e6d688958b
vit-aborigen <vit.aborigen@gmail.com>
2022-07-08 18:13:57 +0300

    init

---
+++ Trigonometry/Trigonometry/TrigonometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrigonometryApp.swift
+//  Trigonometry
+//
+//  Created by VITALI KAZLOU on 8.07.22.
+//
+
+import SwiftUI
+
+@main
+struct TrigonometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
