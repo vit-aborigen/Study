7fd17b4029179b7cb2f33fadb6499525e70e9ff0
vit-aborigen <vit.aborigen@gmail.com>
2022-07-26 23:39:49 +0300

    init

---
+++ ShareSheet/ShareSheet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShareSheet
+//
+//  Created by VITALI KAZLOU on 26.07.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShareSheet/ShareSheet/ShareSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareSheetApp.swift
+//  ShareSheet
+//
+//  Created by VITALI KAZLOU on 26.07.22.
+//
+
+import SwiftUI
+
+@main
+struct ShareSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
