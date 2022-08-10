f1f9fd1b02c0988984724369d94eb8ea674737f7
vit-aborigen <vit.aborigen@gmail.com>
2022-08-10 23:07:32 +0300

    Shadows for bottom sheet added

--- WeatherAPP/WeatherAPP/Views/Navigation/ForecastView.swift
+++ WeatherAPP/WeatherAPP/Views/Navigation/ForecastView.swift
@@ -9,26 +9,27 @@ import SwiftUI
 
 struct ForecastView: View {
     var body: some View {
-        ScrollView {
-            
-        }
-        .backgroundBlur(radius: 25, opaque: true)
-        .background(Color.bottomSheetBackground)
-        .clipShape(RoundedRectangle(cornerRadius: 44))
-        .overlay {
-            Divider()
-                .blendMode(.overlay)
-                .background(Color.bottomSheetBorderTop)
-                .frame(maxHeight: .infinity, alignment: .top)
-                .clipShape(RoundedRectangle(cornerRadius: 44))
-        }
-        .overlay {
-            RoundedRectangle(cornerRadius: 10)
-                .fill(Color.black.opacity(0.3))
-                .frame(width: 48, height: 5)
-                .frame(height: 20)
-                .frame(maxHeight: .infinity, alignment: .top)
-        }
+        ScrollView {}
+            .backgroundBlur(radius: 25, opaque: true)
+            .background(Color.bottomSheetBackground)
+            .clipShape(RoundedRectangle(cornerRadius: 44))
+            .innerShadow(shape: RoundedRectangle(cornerRadius: 44), color: Color.bottomSheetBorderMiddle, lineWidth: 1, offsetX: 0, offsetY: 1, blur: 0, blendMode: .overlay, opacity: 1)
+            .overlay {
+                // Separator
+                Divider()
+                    .blendMode(.overlay)
+                    .background(Color.bottomSheetBorderTop)
+                    .frame(maxHeight: .infinity, alignment: .top)
+                    .clipShape(RoundedRectangle(cornerRadius: 44))
+            }
+            .overlay {
+                // Drag holder
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.black.opacity(0.3))
+                    .frame(width: 48, height: 5)
+                    .frame(height: 20)
+                    .frame(maxHeight: .infinity, alignment: .top)
+            }
     }
 }
 
