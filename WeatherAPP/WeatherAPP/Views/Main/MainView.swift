09f0249dd22fb1119dfe9ad1cfeff81f2cb6dcf3
vit-aborigen <vit.aborigen@gmail.com>
2022-08-12 23:36:37 +0300

    basic animations

--- WeatherAPP/WeatherAPP/Views/Main/MainView.swift
+++ WeatherAPP/WeatherAPP/Views/Main/MainView.swift
@@ -10,52 +10,74 @@ import SwiftUI
 
 struct MainView: View {
     @State private var bottomSheetPosition: BottomSheetPositions = .middle
+    @State private var bottomSheetTranslation: CGFloat = BottomSheetPositions.middle.rawValue
+    @State private var isBottomSheetRolledOut = false
+    
+    var bottomSheetTranslationNormalized: CGFloat {
+        (bottomSheetTranslation - BottomSheetPositions.middle.rawValue) / (BottomSheetPositions.top.rawValue - BottomSheetPositions.middle.rawValue)
+    }
     
     var body: some View {
         NavigationView {
-            ZStack {
-                Color.background
-                    .ignoresSafeArea()
+            GeometryReader { geo in
+                let screenHeight = geo.size.height + geo.safeAreaInsets.top + geo.safeAreaInsets.bottom
+                let imageOffset = screenHeight + 36
                 
-                Image("Background")
-                    .resizable()
-                    .ignoresSafeArea()
-                
-                Image("House")
-                    .frame(maxHeight: .infinity, alignment: .top)
-                    .padding(.top, 257)
-                
-                VStack(spacing: -10) {
-                    Text("Default city")
-                        .font(.largeTitle)
+                ZStack {
+                    Color.background
+                        .ignoresSafeArea()
                     
-                    VStack {
-                        Text(attributedString)
+                    Image("Background")
+                        .resizable()
+                        .ignoresSafeArea()
+                        .offset(y: -bottomSheetTranslationNormalized * imageOffset)
+                    
+                    Image("House")
+                        .frame(maxHeight: .infinity, alignment: .top)
+                        .padding(.top, 257)
+                        .offset(y: -bottomSheetTranslationNormalized * imageOffset)
+                    
+                    VStack(spacing: -10 * (1 - bottomSheetTranslationNormalized)) {
+                        Text("Default city")
+                            .font(.largeTitle)
                         
-                        Text("H:30°\tL:20°")
-                            .font(.title3).fontWeight(.semibold)
+                        VStack {
+                            Text(attributedString)
+                            
+                            Text("H:30°\tL:20°")
+                                .font(.title3).fontWeight(.semibold)
+                                .opacity(1 - bottomSheetTranslationNormalized)
+                        }
+                        
+                        Spacer()
                     }
+                    .padding(.top, 51)
+                    .offset(y: -bottomSheetTranslationNormalized * 46)
                     
-                    Spacer()
-                }
-                .padding(.top, 51)
-                
-                BottomSheetView(position: $bottomSheetPosition) {
-                    // 2do
-                } content: {
-                    ForecastView()
-                }
+                    BottomSheetView(position: $bottomSheetPosition) {
+                        // 2do
+                    } content: {
+                        ForecastView(bottomSheetTranslationNormalized: bottomSheetTranslationNormalized)
+                    }
+                    .onBottomSheetDrag { translation in
+                        bottomSheetTranslation = translation / screenHeight
+                        withAnimation(.easeInOut) {
+                            isBottomSheetRolledOut = bottomSheetPosition == BottomSheetPositions.top ? true : false
+                        }
+                    }
 
-                TabBarView(action: {
-                    bottomSheetPosition = .top
-                })
+                    TabBarView(action: {
+                        bottomSheetPosition = .top
+                    })
+                    .offset(y: bottomSheetTranslationNormalized * 115)
+                }
             }
             .navigationBarHidden(true)
         }
     }
     
     private var attributedString: AttributedString {
-        let stringFromInternet = "23°\nMostly Clear"
+        let stringFromInternet = "23°" + "\(isBottomSheetRolledOut ? " | " : "\n ")" + "Mostly Clear"
         var fullString = AttributedString(stringFromInternet)
         
         let separator: Character = "\n"
@@ -66,13 +88,13 @@ struct MainView: View {
         let weatherState = stringFromInternet.split(separator: separator)
         
         if let temperatureRange = fullString.range(of: weatherState[0]) {
-            fullString[temperatureRange].font = .system(size: 96, weight: .thin)
-            fullString[temperatureRange].foregroundColor = .primary
+            fullString[temperatureRange].font = .system(size: (96 - (bottomSheetTranslationNormalized * (96 - 20))), weight: isBottomSheetRolledOut ? .semibold : .thin)
+            fullString[temperatureRange].foregroundColor = isBottomSheetRolledOut ? .secondary : .primary
         }
         
         if let weatherRange = fullString.range(of: weatherState[1]) {
             fullString[weatherRange].font = .title3.weight(.semibold)
-            fullString[weatherRange].foregroundColor = .secondary
+            fullString[weatherRange].foregroundColor = .secondary.opacity(bottomSheetTranslationNormalized)
         }
         
         return fullString
--- WeatherAPP/WeatherAPP/Views/Navigation/ForecastView.swift
+++ WeatherAPP/WeatherAPP/Views/Navigation/ForecastView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ForecastView: View {
+    var bottomSheetTranslationNormalized: CGFloat = 1
+    
     var body: some View {
         ScrollView {}
             .backgroundBlur(radius: 25, opaque: true)
             .background(Color.bottomSheetBackground)
             .clipShape(RoundedRectangle(cornerRadius: 44))
-            .innerShadow(shape: RoundedRectangle(cornerRadius: 44), color: Color.bottomSheetBorderMiddle, lineWidth: 1, offsetX: 0, offsetY: 1, blur: 0, blendMode: .overlay, opacity: 1)
+            .innerShadow(shape: RoundedRectangle(cornerRadius: 44), color: Color.bottomSheetBorderMiddle, lineWidth: 1, offsetX: 0, offsetY: 1, blur: 0, blendMode: .overlay, opacity: 1 - bottomSheetTranslationNormalized)
             .overlay {
                 // Separator
                 Divider()
