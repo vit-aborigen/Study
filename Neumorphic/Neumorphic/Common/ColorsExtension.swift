184353e4f081d78cbcca8051e43903cd0e871055
vit-aborigen <vit.aborigen@gmail.com>
2022-10-18 22:53:14 +0300

    LightButton done

--- Neumorphic/Neumorphic/Common/ColorsExtension.swift
+++ Neumorphic/Neumorphic/Common/ColorsExtension.swift
@@ -11,6 +11,8 @@ extension Color {
     static let darkShadow = Color.white.opacity(0.7)
     static let lightShadow = Color.black.opacity(0.2)
     static let neumorph = Color(red: 1, green: 1, blue: 0.9)
+    static let darkStart = Color(red: 50 / 255, green: 60 / 255, blue: 65 / 255)
+    static let darkEnd = Color(red: 25 / 255, green: 25 / 255, blue: 30 / 255)
 }
 
 extension LinearGradient {
